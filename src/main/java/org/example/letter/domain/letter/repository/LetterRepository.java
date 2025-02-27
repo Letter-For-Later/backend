05215b6f470545014d414cf95fcba84e6224fcd2
박준규 <junju404@naver.com>
2025-02-28 02:05:09 +0900

    #7 - [FIX] 유저 타입 Long에서 UUID 수정

--- src/main/java/org/example/letter/domain/letter/repository/LetterRepository.java
+++ src/main/java/org/example/letter/domain/letter/repository/LetterRepository.java
@@ -5,8 +5,9 @@ import org.example.letter.domain.letter.entity.LetterStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface LetterRepository extends JpaRepository<Letter, Long> {
-    int countByStatus(LetterStatus status);
-    List<Letter> findAllByStatus(LetterStatus status);
+    int countByStatusAndUserUid(LetterStatus status, UUID userId);
+    List<Letter> findAllByStatusAndUserUid(LetterStatus status, UUID userId);
 } 
--- src/main/java/org/example/letter/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/letter/domain/user/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package org.example.letter.domain.user.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 import org.example.letter.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
-
 }
