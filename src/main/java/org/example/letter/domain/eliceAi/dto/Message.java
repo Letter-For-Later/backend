d796d64ad03c040ab52ac513e13418991cf351a1
Ysw <202021079@sangmyung.kr>
2025-03-01 01:41:58 +0900

    #31 - [FEAT] Message 생성

---
+++ src/main/java/org/example/letter/domain/eliceAi/dto/Message.java
@@ -0,0 +1,19 @@
+package org.example.letter.domain.eliceAi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+    private String role;
+    private String content;
+
+    public Message() {}
+
+    public Message(String role, String content) {
+        this.role = role;
+        this.content = content;
+    }
+
+}
