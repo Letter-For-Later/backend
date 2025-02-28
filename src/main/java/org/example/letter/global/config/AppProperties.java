c1f36dbd4212b0ede324eccfe629de939f63c51b
박준규 <junju404@naver.com>
2025-03-01 00:30:22 +0900

    #11 - [FEAT] prefix 추가

--- src/main/java/org/example/letter/global/config/AppProperties.java
+++ src/main/java/org/example/letter/global/config/AppProperties.java
@@ -5,10 +5,10 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Getter
-@Setter
 @Component
 @ConfigurationProperties(prefix = "app")
+@Getter
+@Setter
 public class AppProperties {
     private String domain;
 }
