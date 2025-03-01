3aa3bccbcb8714cb35b0476eac6f48b7ecf6a4f9
박준규 <junju404@naver.com>
2025-03-01 14:31:20 +0900

    test'

--- src/main/java/org/example/letter/global/config/WebConfig.java
+++ src/main/java/org/example/letter/global/config/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//
+//  dd
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
